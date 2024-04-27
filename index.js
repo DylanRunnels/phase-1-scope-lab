f178179a579e086deb7220cd1511fd09a2ea7f2c
dylan <runnels.dylan@yahoo.com>
2024-04-26 19:26:33 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  console.log(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log(bestCustomer); 
+  }
+  
+  const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; 
+}
+
+console.log(leastFavoriteCustomer); 
+changeLeastFavoriteCustomer(); 
+console.log(leastFavoriteCustomer);
